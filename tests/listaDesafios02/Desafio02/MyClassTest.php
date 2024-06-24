e5486648dd7913dd89a3780a2f115c753b928066
PedroAugustoMuller <pedro.muller.a@gmail.com>
2024-06-24 11:36:14 -0300

    Refatoracao 02

--- tests/listaDesafios02/Desafio02/MyClassTest.php
+++ tests/listaDesafios02/Desafio02/MyClassTest.php
@@ -8,16 +8,5 @@ use Root\Www\ListaDesafios02\Desafio02\MyClass;
 
 class MyClassTest extends TestCase
 {
-    public function testRealizaCompra()
-    {
-        $metodoPagamento = $this->createStub(MetodoPagamento::class);
-        $metodoPagamento->expects($this->once())
-            ->method('processaCompra')
-            ->with($this->equalTo(200))
-            ->willReturn(true);
-        $myClass = new MyClass($metodoPagamento);
-        $myClass->setValorCompra(200);
-        $retorno = $myClass->realizaCompra();
-        $this->assertTrue($retorno);
-    }
+    
 }
