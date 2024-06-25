6967a0534b5f160d7be91a881d9e9b15efa3189c
PedroAugustoMuller <pedro.muller.a@gmail.com>
2024-06-24 23:21:36 -0300

    Desafio02

--- src/ListaDesafios02/Desafio02/MyClass.php
+++ src/ListaDesafios02/Desafio02/MyClass.php
@@ -4,8 +4,47 @@ namespace Root\Www\ListaDesafios02\Desafio02;
 
 class MyClass
 {
-    public function addMethod()
+    private array $methods;
+    private $mockClassName;
+    private $disableOriginalConstructor;
+    private $disableOriginalClone;
+    private $disableAutoload;
+    public function __construct($methods)
     {
+        $this->methods = $methods;
+    }
 
+    public function addMethod($method)
+    {
+        $this->methods[] = $method;
+        return $this->methods;
+    }
+    public function setMockClassName($mockClassName)
+    {
+        $this->mockClassName = $mockClassName;
+        return $this->mockClassName;
+    }
+    public function disableOriginalConstructor()
+    {
+        $this->disableOriginalConstructor = false;
+        return $this->disableOriginalConstructor;
+    }
+    public function disableOriginalClone()
+    {
+        $this->disableOriginalClone = false;
+        return $this->disableOriginalClone();
+    }
+    public function disableAutoload()
+    {
+        $this->disableAutoload = false;
+        return $this->disableAutoload;
+    }
+    public function methodWillReturn10()
+    {
+        return 10;
+    }
+    public function methodWillReturnSelf()
+    {
+        return self::class;
     }
 }
