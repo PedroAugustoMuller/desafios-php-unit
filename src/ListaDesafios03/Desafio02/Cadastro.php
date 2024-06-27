dac98c3dbb38c0252195101a0af9eb6e63e5a799
PedroAugustoMuller <pedro.muller.a@gmail.com>
2024-06-27 11:44:30 -0300

    ListaPronta

--- src/ListaDesafios03/Desafio02/Cadastro.php
+++ src/ListaDesafios03/Desafio02/Cadastro.php
@@ -3,7 +3,7 @@
 namespace Root\Www\ListaDesafios03\Desafio02;
 
 use DateTime;
-use http\Exception\InvalidArgumentException;
+use InvalidArgumentException;
 
 class Cadastro
 {
@@ -14,8 +14,8 @@ class Cadastro
     private string $cpf;
     private string $phoneNumber;
     private string $email;
-    private string $password;
-    private string $confirmPassword;
+    private string $password = 'Ainda não existe';
+    private string $confirmPassword = 'Ainda não existe';
 
     /**
      * @param string $fullName
@@ -28,26 +28,24 @@ class Cadastro
      */
     public function __construct(string $fullName, bool $isForeing, string $birthDate, string $gender, string $cpf, string $phoneNumber, string $email)
     {
-            return $this->validateCadastro($fullName,$isForeing, $birthDate, $gender, $cpf, $phoneNumber, $email);
+        $this->validateCadastro($fullName, $isForeing, $birthDate, $gender, $cpf, $phoneNumber, $email);
     }
-    public function validateCadastro($fullName,$isForeing, $birthDate, $gender, $cpf, $phoneNumber, $email): bool
+
+    public function validateCadastro($fullName, $isForeing, $birthDate, $gender, $cpf, $phoneNumber, $email)
     {
         $birthDate = $this->validateBirthDate($birthDate);
         $cpf = $this->validateCPF($cpf);
         $phoneNumber = $this->validatePhoneNumber($phoneNumber);
-        if(!$birthDate)
-        {
+        if (!$birthDate) {
             throw new InvalidArgumentException('Formado da data inválido - Formato Desejado: dddd/mm/YYYY');
         }
-        if(!$cpf)
-        {
+        if (!$cpf) {
             throw new InvalidArgumentException('Formato de CPF inválido - Formato Desejado: XXX.XXX.XXX-XX');
         }
-        if (!$phoneNumber)
-        {
+        if (!$phoneNumber) {
             throw new InvalidArgumentException('Formato de Telefone Inválido - Formato Desejado: +55XXXXXXXXX');
         }
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             throw new InvalidArgumentException('Email Inválido');
         }
         $this->fullName = $fullName;
@@ -59,40 +57,108 @@ class Cadastro
         $this->email = $email;
         return true;
     }
+
     public function validatePhoneNumber($phoneNumber)
     {
-        if(!preg_match('/^\+55[1-9]\d{10}$/', $phoneNumber))
-        {
+        if (!preg_match('/^\+55\d{2}9\d{8}$/', $phoneNumber)) {
             return false;
         }
         return $phoneNumber;
     }
+
     public function validateBirthDate($birthDate)
     {
-        $format =  'd/m/Y';
-        $dateTimeObject = DateTime::createFromFormat($birthDate,$format);
-        if(!$dateTimeObject || $dateTimeObject->format($format) != $birthDate)
-        {
+        $format = 'd/m/Y';
+        $dateTimeObject = DateTime::createFromFormat($format, $birthDate);
+        if (!$dateTimeObject || $dateTimeObject->format($format) != $birthDate) {
             return false;
         }
         return $dateTimeObject;
     }
+
     public function validateCPF($cpf)
     {
-        $cpf = preg_replace('/\D/', '', $cpf);
-        if (!preg_match('/^\d{3}\.\d{3}\.\d{3}-\d{2}$/', $cpf))
-        {
+        if (!preg_match('/^\d{3}\.\d{3}\.\d{3}-\d{2}$/', $cpf)) {
             return false;
         }
-        return $cpf;
+        $cpfValido = $cpf;
+        $cpf = preg_replace('/[^0-9]/', '', $cpf);
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+        $sum = 0;
+        for ($i = 0; $i < 9; $i++) {
+            $sum += intval($cpf[$i]) * (10 - $i);
+        }
+        $remainder = $sum % 11;
+        $digit1 = ($remainder < 2) ? 0 : (11 - $remainder);
+        if (intval($cpf[9]) !== $digit1) {
+            return false;
+        }
+        $sum = 0;
+        for ($i = 0; $i < 10; $i++) {
+            $sum += intval($cpf[$i]) * (11 - $i);
+        }
+        $remainder = $sum % 11;
+        $digit2 = ($remainder < 2) ? 0 : (11 - $remainder);
+        if (intval($cpf[10]) !== $digit2) {
+            return false;
+        }
+        return $cpfValido;
     }
+
     public function setPassword($password, $confirmPassword)
     {
-        if($password != $confirmPassword)
-        {
+        if ($password != $confirmPassword) {
             throw new InvalidArgumentException('Senhas não coincidem');
         }
         $this->password = $password;
         $this->confirmPassword = $confirmPassword;
+        return true;
+    }
+
+    public function getFullName(): string
+    {
+        return $this->fullName;
+    }
+
+    public function getIsForeing(): bool
+    {
+        return $this->isForeing;
+    }
+
+    public function getBirthDate(): DateTime
+    {
+        return $this->birthDate;
+    }
+
+    public function getGender(): string
+    {
+        return $this->gender;
+    }
+
+    public function getCpf(): string
+    {
+        return $this->cpf;
+    }
+
+    public function getPhoneNumber(): string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getConfirmPassword(): string
+    {
+        return $this->confirmPassword;
     }
 }
